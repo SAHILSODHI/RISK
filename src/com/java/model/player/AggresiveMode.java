d82c3719b7551c930327daae523b64f0dafb7e72
Ghalia Elkerdi <gik992@gmail.com>
2019-04-05 00:53:30 -0400

    implemented attack phase - TODO fix army count deduction bug

--- src/com/java/model/player/AggresiveMode.java
+++ src/com/java/model/player/AggresiveMode.java
@@ -4,14 +4,10 @@ import com.java.model.cards.Card;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Scanner;
+
 
 public class AggresiveMode extends PlayerStrategy {
 
@@ -20,21 +16,211 @@ public class AggresiveMode extends PlayerStrategy {
         super(playerID,playerName);
 
     }
+    
+    private static Scanner input = new Scanner(System.in);
 
-    @Override
-    public void executeReinforcement() {
-
-    }
 
     @Override
     public ArrayList<Card> getValidCards() {
-        return null;
+            ArrayList<Card> playerCardList = getPlayerCardList();
+            ArrayList<Card> playerExchangeCards = new ArrayList<>();
+            ArrayList<Card> cumulatedPlayerExchangeCards = new ArrayList<>();
+
+            System.out.println("*** Cards in hand ***");
+            this.showCards();
+            String userInput = "no";
+            if (playerCardList.size() > 2) {
+                System.out.println("Do you wish to exchange cards ? (yes/no)");
+                userInput = input.nextLine();
+            } else {
+                System.out.println(playerName + " does not have sufficient cards to trade.");
+            }
+            while (!((userInput.toLowerCase().equals("yes")) || (userInput.toLowerCase().equals("no")))) {
+                System.out.println("Please input either yes or no.");
+                userInput = input.nextLine();
+            }
+
+            while ((userInput.equals("yes")) || (userInput.equals("no") && playerCardList.size() > 4)) {
+                if (userInput.equals("no") && (playerCardList.size() > 4)) {
+                    System.out.println("You must exchange cards. You have more than 4 cards in your hand.");
+                }
+                boolean can_exchange = false;
+                this.showCards();
+                Integer cardNumber;
+
+                if (!can_exchange) {
+                    playerExchangeCards = new ArrayList<Card>();
+                    System.out
+                            .println("Please enter three card numbers from the list of the same or different army types.");
+                    for (int i = 0; i < 3; i++) {
+                        cardNumber = input.nextInt();
+                        while (cardNumber >= playerCardList.size()) {
+                            System.out.println("Please input correct number from list");
+                            cardNumber = input.nextInt();
+                        }
+                        playerExchangeCards.add(playerCardList.get(cardNumber));
+                    }
+                    can_exchange = isValidExchange(playerExchangeCards);
+                    if(!can_exchange){
+                        System.out.println("You cannot trade these cards. You should enter three card numbers from the " +
+                                "list of the same or different army types.");
+                        System.out.println("*** Cards in hand ***");
+                        this.showCards();
+                        System.out.println("Do you wish to exchange cards ? (yes/no)");
+                        userInput = input.nextLine();
+                        userInput = input.nextLine();
+                        while (!((userInput.toLowerCase().equals("yes")) || (userInput.toLowerCase().equals("no")))) {
+                            System.out.println("Please input either yes or no.");
+                            userInput = input.nextLine();
+                        }
+                        continue;
+                    }
+                }
+                for (Card card : playerExchangeCards) {
+                    cumulatedPlayerExchangeCards.add(card);
+                    playerCardList.remove(card);
+                }
+                System.out.println("*** Cards in hand ***");
+                this.showCards();
+
+                System.out.println("Do you wish to exchange cards ? (yes/no)");
+                userInput = input.nextLine();
+                while (!((userInput.toLowerCase().equals("yes")) || (userInput.toLowerCase().equals("no")))) {
+                    System.out.println("Please input either yes or no.");
+                    userInput = input.nextLine();
+                }
+            }
+            if ((userInput.equals("no")) && (playerCardList.size() > 2) && (cumulatedPlayerExchangeCards.size() == 0)) {
+                System.out.println(playerName + " does not wish to exchange cards.");
+            }
+            return cumulatedPlayerExchangeCards;
+        }
+
+    /**
+     * The executeAttack() method encompasses the overall attack phase logic and flow
+     * including both single and all-out mode. Attack phase ends when player either
+     * no longer wants to attack, or cannot attack.
+     */
+    @Override
+    public void executeReinforcement() {
+        notifyView();
+
+        ArrayList<Card> playerExchangeCards;
+        playerExchangeCards = getValidCards();
+        Integer totalReinforcementArmyCount = calculateTotalReinforcement(playerExchangeCards);
+
+        ReinforcementPhaseState reinforcementPhase = new ReinforcementPhaseState();
+        reinforcementPhase.setNumberOfArmiesReceived(totalReinforcementArmyCount);
+
+        reinforcementPhaseState.add(reinforcementPhase);
+
+        notifyView();
+        placeArmy(totalReinforcementArmyCount);
     }
+
 
     @Override
     public void placeArmy(Integer reinforcementArmy) {
 
+        Integer currentPlayerID = playerID;
+        HashSet<String> countriesOwned = this.gameData.gameMap.getConqueredCountriesPerPlayer(currentPlayerID);
+
+        System.out.println();
+        System.out.println("**** Reinforcement Phase Begins for player " + this.playerName + "..****\n");
+
+        while (reinforcementArmy > 0) {
+
+            System.out.print(playerName + "'s Total Reinforcement Army Count Remaining -> ["
+                    + String.valueOf(reinforcementArmy) + "]\n");
+
+            /* Information about the countries owned by the player and enemy countries. */
+            for (String countries : countriesOwned) {
+
+                System.out.println("\nCountry owned by " + playerName + "-> " + countries + " & Army Count: "
+                        + this.gameData.gameMap.getCountry(countries).getCountryArmyCount());
+
+                HashSet<String> adjCountries = this.gameData.gameMap.getAdjacentCountries(countries);
+
+                if (adjCountries.isEmpty()) {
+                    System.out.println("No neighboring enemy country for country " + countries);
+                }
+
+                for (String enemyCountries : adjCountries) {
+                    if (this.gameData.gameMap.getCountry(enemyCountries).getCountryConquerorID() != currentPlayerID) {
+                        System.out.println("Neighboring Enemy country name: " + enemyCountries + " & Army Count: "
+                                + this.gameData.gameMap.getCountry(enemyCountries).getCountryArmyCount());
+                    }
+                }
+            }
+            System.out.println("\nEnter the country name to place armies: ");
+            String countryNameByUser = "";
+            do{
+                System.out.println("Please enter the country name to place armies: ");
+                countryNameByUser = input.nextLine();
+            }while (countryNameByUser.equals(""));
+
+
+            /* Check for an invalid country name. */
+            if (this.gameData.gameMap.getCountry(countryNameByUser) == null) {
+                System.out.println("'" + countryNameByUser
+                        + "' is an invalid country name. Please verify the country name from the list.\n\n");
+                continue;
+            }
+
+            /*
+             * Check for a valid country name, but the country belonging to a different
+             * player.
+             */
+            if (this.gameData.gameMap.getCountry(countryNameByUser).getCountryConquerorID() != currentPlayerID) {
+                System.out.println("'" + countryNameByUser
+                        + "' does not belong to you yet!!. Please verify your countries owned from the list below.\n\n");
+                continue;
+            }
+
+            /* Information about armies and placement of armies */
+            System.out.println("Enter the number of armies to be placed, Remaining Army (" + reinforcementArmy + ") :");
+
+            try {
+                Integer numberOfArmiesToBePlacedByUser = Integer.parseInt(input.nextLine());
+
+                if (numberOfArmiesToBePlacedByUser > reinforcementArmy) {
+                    System.out.println("Input value '" + numberOfArmiesToBePlacedByUser
+                            + "' should not be greater than the Total Reinforcement Army Count " + "("
+                            + String.valueOf(reinforcementArmy) + ")\n\n");
+                    continue;
+                }
+
+                if (!(numberOfArmiesToBePlacedByUser > 0)) {
+                    System.out.println("Please input a value greater than 0.\n\n");
+                    continue;
+                }
+
+                System.out.println("Successful...Country chosen " + countryNameByUser + " ,Number of armies placed: "
+                        + numberOfArmiesToBePlacedByUser + "\n\n");
+
+                this.gameData.gameMap.addArmyToCountry(countryNameByUser, numberOfArmiesToBePlacedByUser);
+                reinforcementArmy -= numberOfArmiesToBePlacedByUser;
+
+                ReinforcementPhaseState reinforcementPhase = new ReinforcementPhaseState();
+                reinforcementPhase.setToCountry(countryNameByUser);
+                reinforcementPhase.setNumberOfArmiesPlaced(numberOfArmiesToBePlacedByUser);
+                reinforcementPhaseState.add(reinforcementPhase);
+                notifyView();
+
+            } catch (NumberFormatException ex) {
+                System.out.println(ex.getMessage() + ", please enter numeric values only!\n\n");
+            }
+        }
+        /* End of reinforcement phase, Print the final overview. */
+        System.out.println("Reinforcement Phase is now complete. Here's an overview: \n\n");
+        for (String countries : countriesOwned) {
+            System.out.println("Country owned by you: " + countries + " ,Army Count: "
+                    + this.gameData.gameMap.getCountry(countries).getCountryArmyCount());
+        }
+        reinforcementPhaseState.clear();
+        System.out.println("\n**** Reinforcement Phase Ends for player " + this.playerName + "..****\n");
     }
+
 
     @Override
     public void executeAttack() {
