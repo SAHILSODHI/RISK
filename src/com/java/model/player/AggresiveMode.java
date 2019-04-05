ee375426f32aa5afa93025c69874c73f39b73dcf
Ghalia Elkerdi <gik992@gmail.com>
2019-04-05 00:24:29 -0400

    implemented attack tactic within aggressive mode

--- src/com/java/model/player/AggresiveMode.java
+++ src/com/java/model/player/AggresiveMode.java
@@ -38,7 +38,115 @@ public class AggresiveMode extends PlayerStrategy {
 
     @Override
     public void executeAttack() {
+    	
+    	System.out.println();
+        System.out.println("**** Attack Phase Begins for player " + this.playerName + "..****\n");
+        
+        boolean canAttack = true;
+        boolean hasConnqueredAtleastOneCountry = false;
+        
+        while(canAttack) {
+	        System.out.println("\n" + "Fetching potential attack scenarios for " + this.playerName + "...\n");
+	        
+	        // get all scenarios but we're only interested in attacking with the strongest country & in all-out mode
+	        HashMap<String, ArrayList<String>> potentialAttackScenarios = getPotentialAttackScenarios();
+	        
+	        if (potentialAttackScenarios == null) {
+	            System.out.println(
+	                    "There are currently no attack opportunities for " + this.playerName + ".. Sorry!\n");
+	            System.out.println("\n****Attack Phase Ends for player " + this.playerName + "..****\n");
+	            return;
+	        }
+	
+	        if (potentialAttackScenarios.isEmpty()) {
+	            System.out.println(
+	                    "There are currently no attack opportunities for " + this.playerName + ".. Sorry!\n");
+	            System.out.println("\n****Attack Phase Ends for player " + this.playerName + "..****\n");
+	            return;
+	        }
+	        
+	        AttackPhaseState attackPhase = new AttackPhaseState();
+	        attackPhase.setAttackingPlayer(this.playerName);
+	        attackPhaseState.add(attackPhase);
+	        notifyView();
+	        
+	        // it's not enough to just pick up the strongest country
+	        // we need it to have adjacent enemies to attack
+	        boolean noAdjacentEnemyCountriesToAttack = true;
+	        String strongestCountry = null;
+	        String enemyCountryToAttack = null;
+	        while (noAdjacentEnemyCountriesToAttack) {
+	        	strongestCountry = getStrongestCountryConqueredByPlayer(potentialAttackScenarios);
+	            if(strongestCountry == null) {
+	                System.out.println(
+	                        "There are currently no attack opportunities for " + this.playerName + ".. Sorry!\n");
+	                System.out.println("\n****Attack Phase Ends for player " + this.playerName + "..****\n");
+	                return;
+	            }
+	        	HashSet<String> adjacentCountries = gameData.gameMap.getAdjacentCountries(strongestCountry);
+	        	for(String adjCountry : adjacentCountries){
+	                if(gameData.gameMap.getCountry(adjCountry).getCountryConquerorID() != playerID){
+	               		noAdjacentEnemyCountriesToAttack = false;
+	               		enemyCountryToAttack = adjCountry;
+	               		break;
+	                }
+	            }
+	        	// eliminate strongest country as a possibility before looping back
+	        	if(noAdjacentEnemyCountriesToAttack) {
+	        		potentialAttackScenarios.remove(strongestCountry);
+	        	}      	
+	        }
+	        
+	        if(strongestCountry == null || enemyCountryToAttack == null) {
+	            System.out.println(
+	                    "There are currently no attack opportunities for " + this.playerName + ".. Sorry!\n");
+	            System.out.println("\n****Attack Phase Ends for player " + this.playerName + "..****\n");
+	            return;
+	        }
+	        
+	        attackPhase.setAttackingCountry(strongestCountry);
+	        notifyView();
+	
+	        attackPhase.setDefendingCountry(enemyCountryToAttack);
+	        notifyView();
+	
+	        String defendingPlayer = gameData
+	                .getPlayer(this.gameData.gameMap.getCountry(enemyCountryToAttack).getCountryConquerorID())
+	                .getStrategyType().getPlayerName();
+	        attackPhase.setDefendingPlayer(defendingPlayer);
+	        notifyView();
+	        
+	        while (!attackPhase.getBattleOutcomeFlag()) {
+                if (this.gameData.gameMap.getCountry(strongestCountry).getCountryArmyCount() > 1) {
+                	// proceed with max allowed dice count for both sides
+	                Integer attackerDiceCount = getActualMaxAllowedDiceCountForAction("attack", strongestCountry,3);
+	                attackPhase.setAttackerDiceCount(attackerDiceCount);
+	                Integer defenderDiceCount = getActualMaxAllowedDiceCountForAction("defend",enemyCountryToAttack, 2);
+	                attackPhase.setDefenderDiceCount(defenderDiceCount);
+	                rollDiceBattle(attackPhase);
+                    hasConnqueredAtleastOneCountry = fight(attackPhase) || hasConnqueredAtleastOneCountry;
+                }
 
+            checkIfPlayerHasConqueredTheWorld();
+
+            }
+
+	        if (hasConnqueredAtleastOneCountry) {
+	        	Card card = gameData.cardsDeck.getCard();
+            
+	        	if(card == null) {
+	        		System.out.println("No more cards left in the deck");
+	        	} else {
+	        		this.cardList.add(card);
+	        		System.out.println("PlayerStrategy received 1 card => Army Type: " + card.getArmyType() + ", Country: " + card.getCountry().getCountryName());
+	        		System.out.println("Total cards : " + this.cardList.size());
+	        	}
+	        }
+
+        
+        }
+        
+        endAttack();
     }
 
     @Override
@@ -104,6 +212,14 @@ public class AggresiveMode extends PlayerStrategy {
         }
 
     }
+    
+    /**	
+     * This helper method is utilized by all phases to return,
+     * at any given point in time, the strongest country 
+     * currently conquered by the player
+     * @param potentialScenarios
+     * @return
+     */
     
     public String getStrongestCountryConqueredByPlayer(HashMap<String, ArrayList<String>> potentialScenarios) {
 		
